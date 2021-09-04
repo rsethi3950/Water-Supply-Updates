b6bdca1769518e74eeb814d979fab80f58ed42b1
rsethi3950 <riyasethi941@gmail.com>
2021-09-04 23:38:47 +0530

    completed work

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 import json
+from datetime import datetime
 app = Flask(__name__)
 
 app.secret_key = 'secret key'
@@ -14,18 +15,21 @@ def home():
     with open('db.json', 'r') as openfile:
         json_object = json.load(openfile)
     
-    print(json_object)
     return render_template('index.html', data=json_object)
 
 @app.route('/statusForm', methods =['GET', 'POST'])
 def statusForm():
     if request.method== 'POST':
-        # write to file
-        json_object = json.dumps(request.form, indent = 4)
- 
+        d= request.form.to_dict()
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        d['lastUpdated']= dt_string
+        json_object = json.dumps(d, indent = 4)
         with open("db.json", "w") as outfile:
+            # write to file
             outfile.write(json_object)
         return redirect('/home')
+
     else:
         return render_template('form.html')
 
